823a178ec25e9388090b51015440acd7991b8c32
Garamound <grayling96@gmail.com>
2021-04-03 22:40:17 +0200

    Creating to do list

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("v1/", views.v1, name="view 1"),
+    path("<str:name>", views.index, name="index"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>tech with Garramound</h1>")
-
-def v1(response):
-    return HttpResponse("<h1>View 1</h1>")
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
